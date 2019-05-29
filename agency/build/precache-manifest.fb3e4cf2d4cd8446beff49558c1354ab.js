5c7dda3ec61a31af9b679a239918c62cf675f2ff
Andrey Kornyushko <kap86@ukr.net>
2019-05-29 23:31:38 +0300

    initial commit

---
+++ agency/build/precache-manifest.fb3e4cf2d4cd8446beff49558c1354ab.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "948afb9cff40343f7cfec459f7b6f33a",
+    "url": "/static/media/bg-1-2k.948afb9c.jpg"
+  },
+  {
+    "revision": "f886a9318f70719e7b51",
+    "url": "/static/css/main.92f23ebf.chunk.css"
+  },
+  {
+    "revision": "1097427c8475728f4fdd7fa1b56f85d2",
+    "url": "/static/media/man-laptop-v2.1097427c.svg"
+  },
+  {
+    "revision": "3c25f58335f8129dac4c",
+    "url": "/static/js/1.3c25f583.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12038dd0d371fbb871f993b8c0670759",
+    "url": "/static/media/icons.12038dd0.svg"
+  },
+  {
+    "revision": "6f521e036f39fe3bbd8642e291d320e9",
+    "url": "/static/media/bg-2-t@2x.6f521e03.jpg"
+  },
+  {
+    "revision": "7f1bc2b0c5fe5340f0f1ce80732e2791",
+    "url": "/static/media/bg-2-2k@2x.7f1bc2b0.jpg"
+  },
+  {
+    "revision": "7e65c6e64710e29031347330641335da",
+    "url": "/static/media/bg-2-2k.7e65c6e6.jpg"
+  },
+  {
+    "revision": "c51a8def27c27dea2b63bf1db4ba3d92",
+    "url": "/static/media/bg-2-t.c51a8def.jpg"
+  },
+  {
+    "revision": "6f521e036f39fe3bbd8642e291d320e9",
+    "url": "/static/media/bg-2-d.6f521e03.jpg"
+  },
+  {
+    "revision": "7e65c6e64710e29031347330641335da",
+    "url": "/static/media/bg-2-d@2x.7e65c6e6.jpg"
+  },
+  {
+    "revision": "47bb04f397ea624083651926855290af",
+    "url": "/static/media/bg-1-t.47bb04f3.jpg"
+  },
+  {
+    "revision": "3b9bef268140f6fe3a69399cefbc566f",
+    "url": "/static/media/bg-1-t@2x.3b9bef26.jpg"
+  },
+  {
+    "revision": "3b9bef268140f6fe3a69399cefbc566f",
+    "url": "/static/media/bg-1-d.3b9bef26.jpg"
+  },
+  {
+    "revision": "f886a9318f70719e7b51",
+    "url": "/static/js/main.f886a931.chunk.js"
+  },
+  {
+    "revision": "948afb9cff40343f7cfec459f7b6f33a",
+    "url": "/static/media/bg-1-d@2x.948afb9c.jpg"
+  },
+  {
+    "revision": "a5e64440cd3ce17ca542be43f24bdba7",
+    "url": "/static/media/bg-1-2k@2x.a5e64440.jpg"
+  },
+  {
+    "revision": "b869018abd766707279a284b67704c8d",
+    "url": "/static/media/user-alexander-2x.b869018a.jpg"
+  },
+  {
+    "revision": "4edf24196eac5018cbf9cf02d67b126c",
+    "url": "/static/media/Overpass-SemiBold.4edf2419.ttf"
+  },
+  {
+    "revision": "7e0ee8c45d872446649aacd3ce96e1f7",
+    "url": "/static/media/SourceSansPro-Bold.7e0ee8c4.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "10919850f9b0d50925a13d7db5d3eb21",
+    "url": "/static/media/logo.10919850.svg"
+  },
+  {
+    "revision": "2afa77717943906cc812fb6a7a97aeee",
+    "url": "/static/media/man-mobile.2afa7771.svg"
+  },
+  {
+    "revision": "d882d855ad0c93a45176c40e0dacf8ce",
+    "url": "/static/media/html.d882d855.svg"
+  },
+  {
+    "revision": "2963787969ffcb9cc7324947abf540d5",
+    "url": "/static/media/css.29637879.svg"
+  },
+  {
+    "revision": "cf53a810c6682334aa9a2e46a4b86ce4",
+    "url": "/static/media/javascript.cf53a810.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "3c25f58335f8129dac4c",
+    "url": "/static/css/1.5041dbd2.chunk.css"
+  },
+  {
+    "revision": "5caccd2f00ac4d03f77ba24779794d5a",
+    "url": "/index.html"
+  }
+];
