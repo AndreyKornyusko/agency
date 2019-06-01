075bd940d0d5329f3a382eab6db98b0eee6b4d21
Andrey Kornyushko <kap86@ukr.net>
2019-06-01 23:32:56 +0300

    fixed form validation and form reset

--- agency/build/service-worker.js
+++ agency/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.199b692d8dae503da409aa0051d1b5e9.js"
+  "/precache-manifest.c5a112d331bd51e9668bc5f29f2d4e6f.js"
 );
 
 workbox.clientsClaim();
